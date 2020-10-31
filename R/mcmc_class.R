7630b7b8d9bc9150842febf04b3f855ab31fbf3b
tfojo1 <anthony.fojo@jhmi.edu>
2020-10-31 09:42:50 -0400

    streamlining constructors

--- R/mcmc_class.R
+++ R/mcmc_class.R
@@ -69,162 +69,6 @@ setClass('mcmcsim_chainstate',
              first.step.for.iter='integer'
          ))
 
-##-----------------##
-##-- SUBSET MCMC --##
-##-----------------##
-
-#'@export
-setGeneric("subset.mcmc",
-           def=function(.Object,
-                        chains=1:.Object@n.chains,
-                        additional.burn=0,
-                        additional.thin=1){
-               standardGeneric("subset.mcmc")
-           })
-setMethod("subset.mcmc",
-          signature(.Object='mcmcsim'),
-          def=function(.Object,
-                       chains=1:.Object@n.chains,
-                       additional.burn=0,
-                       additional.thin=1)
-{
-    ## UPDATE THE MCMC OBJECT
-    # Save some initial values
-    orig.thin = .Object@thin
-    orig.burn = .Object@burn
-    orig.n.iter = .Object@n.iter
-    orig.n.chains = .Object@n.chains
-
-    # Call the sub-function and set up indices
-    subsetted = do.subset.mcmc(mcmc=.Object,
-                               chains=chains,
-                               additional.burn=additional.burn,
-                               additional.thin=additional.thin)
-    new.n.iter = subsetted$n.iter
-    new.n.chains = length(chains)
-    n.newly.burned = as.integer(additional.burn * orig.thin)
-    new.burn = .Object@burn + n.newly.burned
-    new.thin = as.integer(.Object@thin * additional.thin)
-
-    keep.indices = (1:orig.n.iter)[subsetted$keep.mask]
-    first.in.thin.group.indices = (1:orig.n.iter)[subsetted$first.in.thin.group.mask]
-    burned.mask = (1:orig.n.iter)<=additional.burn
-    indices.after.last.keep = (1:orig.n.iter)[(1:orig.n.iter)>max(keep.indices)]
-
-    collapsing.matrix = sapply(1:new.n.iter, function(i){
-        col = rep(0, orig.n.iter)
-        col[first.in.thin.group.indices[i]:keep.indices[i]] = 1
-        col
-    })
-
-    # Set up new dim.names
-    iteration.names = new.burn + (1:new.n.iter) * new.thin
-    dim.names.2d = list(chain=1:new.n.chains, iteration=iteration.names)
-    dim.names.3d = list(chain=1:new.n.chains, iteration=iteration.names, variable=.Object@var.names)
-
-    # Update samples
-    .Object@samples = subsetted$samples
-
-    # Update n.iter and n.chains
-    .Object@n.iter = new.n.iter
-    .Object@n.chains = new.n.chains
-
-    # Update thin and burn
-    .Object@burn = new.burn
-    .Object@thin = new.thin
-
-    # Update simulations and simulation indices
-     new.to.old.sim.indices = unique(as.numeric(t(subsetted$simulation.indices)))
-     n.new.sims = length(new.to.old.sim.indices)
-     .Object@simulations = .Object@simulations[new.to.old.sim.indices]
-     .Object@simulation.indices = subsetted$simulation.indices
-     .Object@simulation.indices[] = sapply(.Object@simulation.indices[], function(old.i){
-         (1:n.new.sims)[new.to.old.sim.indices==old.i]
-     })
-
-    # Update log.likelihoods and log.priors
-    .Object@log.likelihoods = .Object@log.likelihoods[chains, keep.indices]
-    .Object@log.priors = .Object@log.priors[chains, keep.indices]
-    dim(.Object@log.likelihoods) = dim(.Object@log.priors) = sapply(dim.names.2d, length)
-    dimnames(.Object@log.likelihoods) = dimnames(.Object@log.priors) = dim.names.2d
-
-    # Update n.accepted.in.burn
-    if (additional.burn>0)
-    {
-        newly.burned = .Object@n.accepted[chains,keep.indices,]
-        if (sum(burned.mask)==1 && new.n.chains==1)
-            newly.burned = sum(newly.burned)
-        else if (new.n.chains==1)
-            newly.burned = colSums(newly.burned)
-        else if (sum(burned.mask)>1)
-            newly.burned = apply(newly.burned, c('chain','step'), sum)
-
-        .Object@n.accepted.in.burn = .Object@n.accepted.in.burn + newly.burned
-    }
-
-    # Update n.accepted
-    #  save this for updating the chain states
-    accepted.after.last.iter = sapply(.Object@sample.steps, function(step){
-        sapply(1:orig.n.chains, function(chain){
-            sum(.Object@n.accepted[chain,indices.after.last.keep,step])
-        })
-    })
-    dim(accepted.after.last.iter) = c(chain=orig.n.chains, step=length(.Object@sample.steps))
-
-    dim.names.accepted = list(chain=1:new.n.chains, iteration=iteration.names, step=.Object@sample.steps)
-
-    .Object@n.accepted = sapply(.Object@sample.steps, function(step){
-        .Object@n.accepted[,,step] %*% collapsing.matrix
-    })
-    dim(.Object@n.accepted) = sapply(dim.names.accepted, length)
-    dimnames(.Object@n.accepted) = dim.names.accepted
-
-    # Update run.times.in.burn
-    run.time.after.last.iter = sapply(1:orig.n.chains, function(chain){
-        sum(.Object@run.times[chain, indices.after.last.keep])
-    })
-
-    if (additional.burn>0)
-    {
-        if (sum(burned.mask)==1)
-            newly.burned = .Object@run.times[chains, burned.mask]
-        else if (new.n.chains==1)
-            newly.burned = sum(.Object@run.times[chains, burned.mask])
-        else
-            newly.burned = rowSums(.Object@run.times[chains, burned.mask])
-
-        .Object@run.times.in.burn = .Object@run.times.in.burn[chains] + newly.burned
-    }
-
-    # Update run.times
-    .Object@run.times = .Object@run.times %*% collapsing.matrix
-    dim(.Object@run.times) = sapply(dim.names.2d, length)
-    dimnames(.Object@run.times) = dim.names.2d
-
-    # Update first.step.for.iter
-    .Object@first.step.for.iter = .Object@first.step.for.iter[chains, first.in.thin.group.indices]
-    dim(.Object@first.step.for.iter) = sapply(dim.names.2d, length)
-    dimnames(.Object@first.step.for.iter) = dim.names.2d
-
-    # Update total.run.time
-    .Object@total.run.time = .Object@total.run.time[chains]
-
-    ## UPDATE THE CHAIN STATES
-    .Object@chain.states = lapply(chains, function(chain){
-        chain.state = .Object@chain.states[[chain]]
-
-        chain.state@n.unthinned.after.burn = chain.state@n.unthinned.after.burn - n.newly.burned
-        chain.state@n.unthinned.burned = chain.state@n.unthinned.burned + n.newly.burned
-
-        chain.state@n.accepted = chain.state@n.accepted + accepted.after.last.iter[chain,]
-        chain.state@run.time = chain.state@run.time + run.time.after.last.iter[chain]
-
-        chain.state
-    })
-
-    .Object
-})
-
 
 ##-------------------------------------------------------------------##
 ##-- CLASS METHODS THAT WILL BE IMPLEMENTED AT THE SUB-CLASS LEVEL --##
@@ -389,3 +233,160 @@ setMethod("initialize",
                    ...)
 })
 
+
+
+##-----------------##
+##-- SUBSET MCMC --##
+##-----------------##
+
+#'@export
+setGeneric("subset.mcmc",
+           def=function(.Object,
+                        chains=1:.Object@n.chains,
+                        additional.burn=0,
+                        additional.thin=1){
+               standardGeneric("subset.mcmc")
+           })
+setMethod("subset.mcmc",
+          signature(.Object='mcmcsim'),
+def=function(.Object,
+           chains=1:.Object@n.chains,
+           additional.burn=0,
+           additional.thin=1)
+{
+    ## UPDATE THE MCMC OBJECT
+    # Save some initial values
+    orig.thin = .Object@thin
+    orig.burn = .Object@burn
+    orig.n.iter = .Object@n.iter
+    orig.n.chains = .Object@n.chains
+
+    # Call the sub-function and set up indices
+    subsetted = do.subset.mcmc(mcmc=.Object,
+                               chains=chains,
+                               additional.burn=additional.burn,
+                               additional.thin=additional.thin)
+    new.n.iter = subsetted$n.iter
+    new.n.chains = length(chains)
+    n.newly.burned = as.integer(additional.burn * orig.thin)
+    new.burn = .Object@burn + n.newly.burned
+    new.thin = as.integer(.Object@thin * additional.thin)
+
+    keep.indices = (1:orig.n.iter)[subsetted$keep.mask]
+    first.in.thin.group.indices = (1:orig.n.iter)[subsetted$first.in.thin.group.mask]
+    burned.mask = (1:orig.n.iter)<=additional.burn
+    indices.after.last.keep = (1:orig.n.iter)[(1:orig.n.iter)>max(keep.indices)]
+
+    collapsing.matrix = sapply(1:new.n.iter, function(i){
+        col = rep(0, orig.n.iter)
+        col[first.in.thin.group.indices[i]:keep.indices[i]] = 1
+        col
+    })
+
+    # Set up new dim.names
+    iteration.names = new.burn + (1:new.n.iter) * new.thin
+    dim.names.2d = list(chain=1:new.n.chains, iteration=iteration.names)
+    dim.names.3d = list(chain=1:new.n.chains, iteration=iteration.names, variable=.Object@var.names)
+
+    # Update samples
+    .Object@samples = subsetted$samples
+
+    # Update n.iter and n.chains
+    .Object@n.iter = new.n.iter
+    .Object@n.chains = new.n.chains
+
+    # Update thin and burn
+    .Object@burn = new.burn
+    .Object@thin = new.thin
+
+    # Update simulations and simulation indices
+    new.to.old.sim.indices = unique(as.numeric(t(subsetted$simulation.indices)))
+    n.new.sims = length(new.to.old.sim.indices)
+    .Object@simulations = .Object@simulations[new.to.old.sim.indices]
+    .Object@simulation.indices = subsetted$simulation.indices
+    .Object@simulation.indices[] = sapply(.Object@simulation.indices[], function(old.i){
+        (1:n.new.sims)[new.to.old.sim.indices==old.i]
+    })
+
+    # Update log.likelihoods and log.priors
+    .Object@log.likelihoods = .Object@log.likelihoods[chains, keep.indices]
+    .Object@log.priors = .Object@log.priors[chains, keep.indices]
+    dim(.Object@log.likelihoods) = dim(.Object@log.priors) = sapply(dim.names.2d, length)
+    dimnames(.Object@log.likelihoods) = dimnames(.Object@log.priors) = dim.names.2d
+
+    # Update n.accepted.in.burn
+    if (additional.burn>0)
+    {
+        newly.burned = .Object@n.accepted[chains,keep.indices,]
+        if (sum(burned.mask)==1 && new.n.chains==1)
+            newly.burned = sum(newly.burned)
+        else if (new.n.chains==1)
+            newly.burned = colSums(newly.burned)
+        else if (sum(burned.mask)>1)
+            newly.burned = apply(newly.burned, c('chain','step'), sum)
+
+        .Object@n.accepted.in.burn = .Object@n.accepted.in.burn + newly.burned
+    }
+
+    # Update n.accepted
+    #  save this for updating the chain states
+    accepted.after.last.iter = sapply(.Object@sample.steps, function(step){
+        sapply(1:orig.n.chains, function(chain){
+            sum(.Object@n.accepted[chain,indices.after.last.keep,step])
+        })
+    })
+    dim(accepted.after.last.iter) = c(chain=orig.n.chains, step=length(.Object@sample.steps))
+
+    dim.names.accepted = list(chain=1:new.n.chains, iteration=iteration.names, step=.Object@sample.steps)
+
+    .Object@n.accepted = sapply(.Object@sample.steps, function(step){
+        .Object@n.accepted[,,step] %*% collapsing.matrix
+    })
+    dim(.Object@n.accepted) = sapply(dim.names.accepted, length)
+    dimnames(.Object@n.accepted) = dim.names.accepted
+
+    # Update run.times.in.burn
+    run.time.after.last.iter = sapply(1:orig.n.chains, function(chain){
+        sum(.Object@run.times[chain, indices.after.last.keep])
+    })
+
+    if (additional.burn>0)
+    {
+        if (sum(burned.mask)==1)
+            newly.burned = .Object@run.times[chains, burned.mask]
+        else if (new.n.chains==1)
+            newly.burned = sum(.Object@run.times[chains, burned.mask])
+        else
+            newly.burned = rowSums(.Object@run.times[chains, burned.mask])
+
+        .Object@run.times.in.burn = .Object@run.times.in.burn[chains] + newly.burned
+    }
+
+    # Update run.times
+    .Object@run.times = .Object@run.times %*% collapsing.matrix
+    dim(.Object@run.times) = sapply(dim.names.2d, length)
+    dimnames(.Object@run.times) = dim.names.2d
+
+    # Update first.step.for.iter
+    .Object@first.step.for.iter = .Object@first.step.for.iter[chains, first.in.thin.group.indices]
+    dim(.Object@first.step.for.iter) = sapply(dim.names.2d, length)
+    dimnames(.Object@first.step.for.iter) = dim.names.2d
+
+    # Update total.run.time
+    .Object@total.run.time = .Object@total.run.time[chains]
+
+    ## UPDATE THE CHAIN STATES
+    .Object@chain.states = lapply(chains, function(chain){
+        chain.state = .Object@chain.states[[chain]]
+
+        chain.state@n.unthinned.after.burn = chain.state@n.unthinned.after.burn - n.newly.burned
+        chain.state@n.unthinned.burned = chain.state@n.unthinned.burned + n.newly.burned
+
+        chain.state@n.accepted = chain.state@n.accepted + accepted.after.last.iter[chain,]
+        chain.state@run.time = chain.state@run.time + run.time.after.last.iter[chain]
+
+        chain.state
+    })
+
+    .Object
+})
